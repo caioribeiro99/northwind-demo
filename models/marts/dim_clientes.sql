2e69d19efd9a713647f0a70d90680da7827c04cc
Caio Tertuliano Ribeiro <caio.tertuliano@indicium.tech>
2023-06-28 10:48:04 +0000

    Finalizado as dimensões cliente e produto

---
+++ models/marts/dim_clientes.sql
@@ -0,0 +1,22 @@
+WITH
+    clientes AS (
+        SELECT *
+        FROM {{ ref('stg_erp__clientes') }}
+    )
+
+    , transformacoes AS (
+        SELECT
+        row_number() OVER (ORDER BY id_cliente) AS sk_cliente
+        , id_cliente
+        , nome_cliente
+        , empresa_cliente
+        , endereco_cliente
+        , cep_cliente
+        , cidade_cliente
+        , regiao_cliente
+        , pais_cliente
+    FROM clientes
+    )
+
+SELECT *
+FROM transformacoes
--- models/stage/erp/stg_erp__produtos.sql
+++ models/stage/erp/stg_erp__produtos.sql
@@ -5,7 +5,7 @@ WITH
             , cast(supplier_id AS int) AS id_fornecedor
             , cast(category_id AS int) AS id_categoria
             , cast(product_name AS string) AS nome_produto
-            , cast(quantity_per_unit AS string) AS quantiade_por_unidade
+            , cast(quantity_per_unit AS string) AS quantidade_por_unidade
             , cast(unit_price AS numeric) AS preco_por_unidade
             , cast(units_in_stock AS int) AS unidades_em_estoque
             , cast(units_on_order AS int) AS unidades_por_ordem
